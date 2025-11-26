3148273060b527130a370256d3ce6e9bf4d11917
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-11-26 12:05:42 +0600

    delete dist

--- dist/app/config/redis.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConnection = void 0;
-const redis_1 = require("redis");
-exports.redisConnection = (0, redis_1.createClient)({
-    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
-});
-exports.redisConnection.on('error', (err) => console.error('Redis Client Error', err));
-exports.redisConnection.connect();
--- dist/enums/service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVICE_STATUS = void 0;
-var SERVICE_STATUS;
-(function (SERVICE_STATUS) {
-    SERVICE_STATUS["OLD"] = "Old";
-    SERVICE_STATUS["NEW"] = "New";
-    SERVICE_STATUS["EDITED"] = "Edited";
-})(SERVICE_STATUS || (exports.SERVICE_STATUS = SERVICE_STATUS = {}));
