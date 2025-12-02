d565c50f4247c078dd479cc9b293060e6015a9b9
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-12-02 14:08:40 +0600

    redis command line

--- src/app/config/redis.ts
+++ src/app/config/redis.ts
@@ -9,3 +9,13 @@ redisConnection.on('error', (err) =>
 );
 
 redisConnection.connect();
+
+// --------------------- Redis Connection Commands --------------------- //
+// sudo apt update
+// sudo apt install redis-server
+// sudo service redis-server start
+// sudo systemctl enable redis-server
+
+// sudo service redis-server stop
+// sudo service redis-server restart
+// sudo service redis-server status
